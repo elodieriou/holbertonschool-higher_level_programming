41adae9c76cd3a61a565eab0a2dd1e58c0e6287b
Elodie Riou <elodie.riou@hotmail.com>
2021-12-21 12:16:14 +0100

    fix code with format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for n1 in range(0, 10):
     for n2 in range(0, 10):
         if n1 < n2 and n1 < 8:
-            print(n1, end="")
-            print(n2, end=", ")
+            print("{:d}".format(n1), end="")
+            print("{:d}".format(n2), end=", ")
 print("89")
