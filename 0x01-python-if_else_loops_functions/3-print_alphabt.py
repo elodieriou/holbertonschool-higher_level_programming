9b5543a5476ea7951a40d0fd46a04207dc6bb2d9
Elodie Riou <elodie.riou@hotmail.com>
2021-12-21 12:08:33 +0100

    fix code with format and not chr

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabet in range(97, 123):
     if alphabet != 113 and alphabet != 101:
-        print(chr(alphabet), end="")
+        print("{:c}".format(alphabet), end="")
