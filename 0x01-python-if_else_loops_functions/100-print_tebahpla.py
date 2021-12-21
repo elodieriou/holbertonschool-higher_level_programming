3750edc02344123f4edaa4d3eeb985161b70e5a0
Elodie Riou <elodie.riou@hotmail.com>
2021-12-21 15:05:17 +0100

    fix code with if not

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for alphabet in range(122, 96, -1):
-    if (alphabet % 2) != 0:
-        upper = alphabet - 32
-    else:
+    if not alphabet % 2:
         upper = alphabet
+    else:
+        upper = alphabet - 32
     print("{}".format(chr(upper)), end="")
