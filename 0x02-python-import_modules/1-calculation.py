f5315818be0f8343ce138f07deaf396c44c86e82
Elodie Riou <elodie.riou@hotmail.com>
2021-12-23 12:06:06 +0100

    fix code with not possible to import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,3 +6,9 @@ print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
 print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+if __name__ == '__main__':
+    add(a, b)
+    sub(a, b)
+    mul(a, b)
+    div(a, b)
