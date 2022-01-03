d966181cf6dc8c29858ff09943df97bea03644b8
Elodie Riou <elodie.riou@hotmail.com>
2022-01-03 17:00:17 +0100

    fix code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        string_len = len(sentence)
+    string_len = len(sentence)
+    if sentence == "":
+        first_char = None
+    else:
         first_char = sentence[0]
     return string_len, first_char
