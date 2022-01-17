1072ee27d3e07250b7a77a8d77078094353f01bc
Elodie Riou <elodie.riou@hotmail.com>
2022-01-17 16:12:37 +0100

    add the Python function magic calculation (a, b)

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            result = a ** b + i;
+            result = a + b;
+        except:
+            raise NameError('Too far')
+        finally:
+            break
+    return result
