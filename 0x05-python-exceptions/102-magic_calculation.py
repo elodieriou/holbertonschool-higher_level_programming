8e0fb29e8ccffcae3f4952167ce1de06398048d6
Elodie Riou <elodie.riou@hotmail.com>
2022-01-17 16:29:14 +0100

    fix code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,10 +3,12 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            result = a ** b + i;
-            result = a + b;
-        except:
-            raise NameError('Too far')
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result = a ** b / i
+        except Exception:
+            result = a + b
         finally:
             break
     return result
