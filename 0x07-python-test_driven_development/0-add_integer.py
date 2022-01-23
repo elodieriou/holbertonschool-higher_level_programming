14e1e34ec094e3c935b8359ad5635a2e65b92903
Elodie Riou <elodie.riou@hotmail.com>
2022-01-23 13:56:25 +0100

    add files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+The function add_integer.
+"""
+def add_integer(a, b=98):
+    """
+    The function adds 2 integers.
+
+    Args:
+        - a (int or float):
+            . must an integer or float
+            . if it's a float, caste to integers
+        - b (int or float):
+            . initialize to 98
+            . must an integer or float
+            . if it's a float, caste to integers
+
+    Return:
+        the addition of a and b
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError('a must be an integer')
+    elif type(b) != int and type(b) != float:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
