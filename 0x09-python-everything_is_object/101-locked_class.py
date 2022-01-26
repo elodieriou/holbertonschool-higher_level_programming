664bd58d916603359b5b07e90c1e0c0da578b6bc
Elodie Riou <elodie.riou@hotmail.com>
2022-01-26 17:31:46 +0100

    fix code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-class LockedClass:
+"""
+The class LockedClass is a class Object. When called, it accepts no arguments
+and returns a new featureless instance that has no instance attributes and
+cannot be given any.
+"""
+
+
+class LockedClass(object):
+    """
+    The class attribute __slots__ is called. slots provide a special mechanism
+    to reduce the size of objects. It is a concept of memory optimisation on
+    objects.
+    """
     __slots__ = 'first_name'
