b06dda3006029081f831c888287b5f514062d9ad
Elodie Riou <elodie.riou@hotmail.com>
2022-02-01 17:44:07 +0100

    add 11-file

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -59,5 +59,5 @@ class Student:
                 . A dictionary key will be the public attribute name
                 . A dictionary value will be the value of the public attribute
         """
-        for key, value in json.item():
-            self.__dict__[key] = value
+        for element in json:
+            self.__dict__[element] = json[element]
