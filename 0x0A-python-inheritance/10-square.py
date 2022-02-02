64c0e1ec9b323ed89894f31dc6f8e91d77a23029
Elodie Riou <elodie.riou@hotmail.com>
2022-02-02 16:45:47 +0100

    test

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""
-The module define a subclass Square inherits from Rectangle inherits
-from BaseGeometry:
-    * Instantiation with size: def __init__(self, size)::
-        . size must be private. No getter or setter
-        . size must be a positive integer, validated by integer_validator
-    * the area() method must be implemented
-    * print() should print, and str() should return, the square description:
-      [Square] <width>/<height>
+"""Write a class Square that inherits from Rectangle
+size must be private. No getter or setter
+size must be a positive integer, validated by integer_validator
+the area() method must be implemented
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    The class Square define:
-        * Instantiation with size: def __init__(self, size)
-        * the area() method must be implemented
-    """
+    """ defined a square of size "size by size" """
+
     def __init__(self, size):
-        """
-        The initialization method of class Square.
-        Args:
-            - __size (int, private)
-        """
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
+        """function that compute the area of the square"""
+
     def area(self):
-        """
-        The method that compute the area of a square.
-        """
-        return self.__size * self.__size
+        return self.__size ** 2
