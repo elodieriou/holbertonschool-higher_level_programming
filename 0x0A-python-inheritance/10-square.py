c67dd1bb3d5cc91618483a2ade5f94b7eedc04d4
Elodie Riou <elodie.riou@hotmail.com>
2022-02-02 16:48:19 +0100

    tests

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,18 +6,16 @@ the area() method must be implemented
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
-""" create a class square of inheritance from Rectangle"""
-
 
 class Square(Rectangle):
     """ defined a square of size "size by size" """
 
     def __init__(self, size):
+        """ defined a square of size "size by size" """
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
-        """function that compute the area of the square"""
-
     def area(self):
+        """function that compute the area of the square"""
         return self.__size ** 2
