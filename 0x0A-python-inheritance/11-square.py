31a59477f785d34af9435221e621118cad6a62f8
Elodie Riou <elodie.riou@hotmail.com>
2022-02-02 16:56:07 +0100

    tests without from

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
-"""Write a class Square that inherits from Rectangle
-size must be private. No getter or setter
-size must be a positive integer, validated by integer_validator
-the area() method must be implemented
-print() should print, and str() should return
-the square description: [Square] <width>/<height>
 """
-
+The module define a subclass Square inherits from Rectangle inherits
+BaseGeometry:
+    * Instantiation with size: def __init__(self, size)::
+        . size must be private. No getter or setter
+        . size must be a positive integer, validated by integer_validator
+    * the area() method must be implemented
+    * print() should print, and str() should return, the square description:
+      [Square] <width>/<height>
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ defined a square of size "size by size" """
-
-    """__init__ method to initialize a new instance"""
+    """
+    The class Square define:
+        * Instantiation with size: def __init__(self, size)
+        * the area() method must be implemented
+    """
     def __init__(self, size):
+        """
+        The initialization method of class Square.
+        Args:
+            - __size (int, private)
+        """
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
-    """area method to compute the area of the rectangle"""
-
     def area(self):
-        return self.__size ** 2
-
-    """str method to print the rectangle size"""
+        """
+        The method that compute the area of a square.
+        """
+        return self.__size * self.__size
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """
+        The method __str__ print and return the string representation of main.
+        """
+        w = self.__size
+        h = self.__size
+        return "[Square] {:d}/{:d}".format(w, h)
