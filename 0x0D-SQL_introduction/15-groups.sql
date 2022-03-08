3bf6ca71a55b1f52e081888cfd57e2ad1520cf67
Elodie Riou <elodie.riou@hotmail.com>
2022-03-08 13:31:16 +0100

    add file for the task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- list the number of records with the same score in the table 'second_table'
+-- SELECT .. AS .. FROM .. GROUP BY .. ORDER BY ..
+SELECT score, COUNT(score) AS number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
