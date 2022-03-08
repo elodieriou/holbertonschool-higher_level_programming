f4ff4de6d6e0081aafce9e7db10c12fa304afeb4
Elodie Riou <elodie.riou@hotmail.com>
2022-03-08 12:43:40 +0100

    add file for task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- list all records of the table second_table displaying score and name, ordered by score
+-- SELECT .. FROM .. ORDER BY .. DESC
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
