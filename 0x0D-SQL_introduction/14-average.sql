4b720039e07d341f37903f65d61902d47f10ce2b
Elodie Riou <elodie.riou@hotmail.com>
2022-03-08 13:18:24 +0100

    add file for the task 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- compute the score average of all records in the table 'second_table' and add the result to the 'average' column
+-- SELECT .. AS .. FROM ..
+SELECT AVG(score) AS average FROM second_table;
