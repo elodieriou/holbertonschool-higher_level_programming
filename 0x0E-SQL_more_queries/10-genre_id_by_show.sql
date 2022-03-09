3b1bc17b39543c1dde88ac6174af137ea6d29fd0
Elodie Riou <elodie.riou@hotmail.com>
2022-03-09 14:46:51 +0100

    fix code

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- list all shows contained in 'hbtn_0d_tvshows'
---
+-- RIGHT JOIN
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
