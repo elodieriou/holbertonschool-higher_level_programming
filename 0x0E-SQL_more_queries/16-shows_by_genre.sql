30ab8d8255f2ddfa36f8c90afd0238e2cd94fb36
Elodie Riou <elodie.riou@hotmail.com>
2022-03-09 16:47:29 +0100

    fix code

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER by tv_shows.title;
+ORDER by tv_shows.title, tv_genres.name;
