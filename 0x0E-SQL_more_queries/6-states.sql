87f8a0ccb9c528d0a77043399897810f552a0723
Elodie Riou <elodie.riou@hotmail.com>
2022-03-09 12:00:08 +0100

    add file for task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create the database 'hbtn_0d_usa' and the table 'states'
+-- CREATE DATABASE / TABLE
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
+);
