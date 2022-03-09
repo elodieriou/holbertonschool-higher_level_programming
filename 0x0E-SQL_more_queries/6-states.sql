579dd1c14c82aad7872e312daa40e509d4c71259
Elodie Riou <elodie.riou@hotmail.com>
2022-03-09 12:05:49 +0100

    update with USE

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- create the database 'hbtn_0d_usa' and the table 'states'
 -- CREATE DATABASE / TABLE
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
        name VARCHAR(256) NOT NULL
 );
