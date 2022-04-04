f49962aab8e743650474817cdad0cead61170a03
Elodie Riou <elodie.riou@hotmail.com>
2022-04-04 15:23:43 +0200

    fix style

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function add(a, b) {
-    console.log(parseInt(a) + parseInt(b));
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
 }
 add(parseInt(process.argv[2], 10), parseInt(process.argv[3], 10));
