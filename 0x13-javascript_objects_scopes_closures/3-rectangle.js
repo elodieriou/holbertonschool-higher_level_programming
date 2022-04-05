78aad5beb56de0f3e8497128a5cba30555b763fa
Elodie Riou <elodie.riou@hotmail.com>
2022-04-05 10:46:50 +0200

    add file for task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+};
