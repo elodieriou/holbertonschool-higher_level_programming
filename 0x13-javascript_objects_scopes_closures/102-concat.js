65a48e4800613916234aa9f67b596bd7687e6cb8
Elodie Riou <elodie.riou@hotmail.com>
2022-04-06 10:26:10 +0200

    add file for task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv;
+const fs = require('fs');
+const dataFileA = fs.readFileSync(args[2], 'utf8');
+const dataFileB = fs.readFileSync(args[3], 'utf8');
+const content = dataFileA + dataFileB;
+fs.writeFile(args[4], content, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
