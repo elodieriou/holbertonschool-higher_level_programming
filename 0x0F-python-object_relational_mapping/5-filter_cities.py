80bdb8dadc8afc3aa983f5fc87736530b6994fee
Elodie Riou <elodie.riou@hotmail.com>
2022-04-07 14:13:52 +0200

    update comment

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that lists all cities from the database hbtn_0e_4_usa"""
+"""script that takes in the name of a state as an argument
+ and lists all cities of that state, using the database hbtn_0e_4_usa"""
 import MySQLdb
 from sys import argv
 
