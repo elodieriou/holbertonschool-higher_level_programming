724359ed0f06ade5e496ce75605e970cd6c01bf7
Elodie Riou <elodie.riou@hotmail.com>
2022-04-07 10:27:29 +0200

    add file for task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3], charset="utf8")
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cursor.fetchall()
+for row in query_rows:
+    print(row)
+cursor.close()
+connection.close()
+
+if __name__ == "__main__":
+    MySQLdb
+    argv
