93a2847fe32e0a039e0ecd7686ffcabf7304bd26
Elodie Riou <elodie.riou@hotmail.com>
2022-04-07 19:24:51 +0200

    add file for task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains the class definition of a City and an instance
+ Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """The class City"""
+    __tablename__ = 'cities'
+    id = Column(Integer,  nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False, )
