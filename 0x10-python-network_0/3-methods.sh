7f8acaa4c57a46cedccb89e0202f2364a7fa550a
Elodie Riou <elodie.riou@hotmail.com>
2022-04-21 13:07:34 +0200

    add file for task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -sIX OPTIONS https://google.com | grep allow | cut -d " " -f 2-
+curl -sIX OPTIONS "$1" | grep allow | cut -d " " -f 2-
