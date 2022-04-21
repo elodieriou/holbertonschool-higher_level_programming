7b4683cd0b210b6c4b2a9cd08f38981447515787
Elodie Riou <elodie.riou@hotmail.com>
2022-04-21 13:31:17 +0200

    fix code

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "X-School-User-Id: 80" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
