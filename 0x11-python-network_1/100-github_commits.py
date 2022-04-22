3e631c4b15229cec00d4dff4fa631d3e6a55af8a
Elodie Riou <elodie.riou@hotmail.com>
2022-04-22 17:12:55 +0200

    fix code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,11 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     req = requests.get(url)
     my_dict = req.json()
-    for i in range(0, 10):
-        sha = my_dict[i].get("sha")
-        name = my_dict[i].get("commit").get("author").get("name")
+    number_commit = 0
+    for commits in my_dict:
+        if number_commit == 10:
+            break
+        sha = commits.get("sha")
+        name = commits.get("commit").get("author").get("name")
         print("{}: {}".format(sha, name))
+        number_commit += 1
