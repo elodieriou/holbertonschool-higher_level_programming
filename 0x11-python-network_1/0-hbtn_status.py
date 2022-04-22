8faccc2a958855696536caf08010d4a560d8299c
Elodie Riou <elodie.riou@hotmail.com>
2022-04-22 10:09:22 +0200

    add file for task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""THis module defines a script that fetches https://intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    
+    url = "https://intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        page = response.read()
+        utf = page.decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(utf))
