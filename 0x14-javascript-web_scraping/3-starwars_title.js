8ca69672e7c7255af8b69f3d4ff2bce91399fee1
Elodie Riou <elodie.riou@hotmail.com>
2022-05-17 12:06:10 +0200

    add file for task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const episode = process.argv[2];
+axios
+  .get(url + episode)
+  .then(function (response) {
+    console.log(response.data.title);
+  })
+  .catch(function (error) {
+    console.log('An error has occurred. Code: ' + error.response.status);
+  })
+  .then(function () {
+  });
