f71d155757e4f217149b739c068741613064079f
Elodie Riou <elodie.riou@hotmail.com>
2022-05-17 15:11:24 +0200

    fix semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 const args = process.argv;
-try {
-  fs.writeFile(args[2], args[3], 'utf8', function (error) {
-  });
-}
-catch(error) {
-  console.log(error);
-}
+fs.writeFile(args[2], args[3], 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
