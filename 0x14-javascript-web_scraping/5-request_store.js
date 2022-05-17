fc9803c66b00111dae5c68010585e14a867f95fc
Elodie Riou <elodie.riou@hotmail.com>
2022-05-17 15:28:40 +0200

    add file for task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const axios = require('axios').default;
+
+axios
+  .get(process.argv[2])
+  .then(function (response) {
+    fs.writeFile(process.argv[3], response.data, 'utf8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  });
