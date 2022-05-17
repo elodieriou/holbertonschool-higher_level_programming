766d56ea8a2703f8485a4f4149fd7b040b701324
Elodie Riou <elodie.riou@hotmail.com>
2022-05-17 18:04:57 +0200

    add file for task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+axios
+  .get(url)
+  .then(function (response) {
+    const numberCharacters = response.data.characters.length;
+    for (let i = 0; i < numberCharacters; i++) {
+      const characters = response.data.characters[i];
+      axios
+        .get(characters)
+        .then(function (response) {
+          console.log(response.data.name);
+        })
+        .catch(function (error) {
+          console.log('An error has occurred - ' + error);
+        })
+        .then(function () {
+        });
+    }
+  })
+  .catch(function (error) {
+    console.log('An error has occurred - ' + error);
+  })
+  .then(function () {
+  });
