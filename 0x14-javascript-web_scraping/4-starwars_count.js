784f062af692896a9891e5384fce3247c030c3a3
Elodie Riou <elodie.riou@hotmail.com>
2022-05-17 17:39:07 +0200

    fix semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,9 @@ axios
     let count = 0;
     const numberFilm = response.data.results.length;
     for (let i = 0; i < numberFilm; i++) {
-      const numberCharacters = response.data.results[i].characters.length
+      const numberCharacters = response.data.results[i].characters.length;
       for (let j = 0; j < numberCharacters; j++) {
-        const filmForCharacter = response.data.results[i].characters[j]
+        const filmForCharacter = response.data.results[i].characters[j];
         if (filmForCharacter.includes('18') === true) {
           count += 1;
         }
